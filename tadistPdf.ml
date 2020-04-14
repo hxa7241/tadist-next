7add6d9d3b60a3745bc6306a2e96a24fcca772f0
hxa7241 <>
2020-04-14 10:13:11 +0000

    [add] (TadistPdf) Implemented basic title, author, date metadata extraction.
    FossilOrigin-Name: 651455519ca1f77cbada7efb738b7b30e95ca96d764e9265cfec8b110d51de57

--- tadistPdf.ml
+++ tadistPdf.ml
@@ -14,7 +14,7 @@
 
 open HxaGeneral
 (* using Tadist *)
-(* using Pdf, Pdfread, Pdfpage *)
+(* using Camlpdf: Pdf, Pdfpage, Pdfread, Pdftext *)
 
 
 
@@ -26,43 +26,93 @@ let _TYPE = "pdf"
 
 
 
+(* ---- functions ---- *)
+
+let recognisePdf (pdfPathname:string) : bool ress =
+
+   try
+      (* (assuming this can fail) *)
+      let file = open_in_bin pdfPathname in
+
+      let recognised =
+         let readString file pos len =
+            (* (assuming these cannot fail, in a deeper IO sense) *)
+            try seek_in file pos ; really_input_string file len with _ -> ""
+         in
+         (* check pdf id ("%PDF-") *)
+         (readString file 0 5) = "\x25\x50\x44\x46\x2D"
+      in
+
+      close_in_noerr file ;
+      Ok recognised
+   with
+   | _ -> Error ("cannot open/read file: " ^ pdfPathname)
+
+
+(*
+ * Lookup an entry from the /Info dictionary, returning it as a UTF8 string
+ *)
+let lookupInfoUtf8 (pdf:Pdf.t) (key:string) : string =
+
+   let infodict =
+      match Pdf.lookup_direct pdf "/Info" pdf.Pdf.trailerdict with
+      | Some infodict -> infodict
+      | None          -> Pdf.Dictionary []
+   in
+
+   match Pdf.lookup_direct pdf key infodict with
+   | Some (Pdf.String s) -> Pdftext.utf8_of_pdfdocstring s
+   | Some
+      Pdf.(Null | Boolean _ | Integer _ |
+         Real _ | Name _ | Array _ |
+         Dictionary _ | Stream _ | Indirect _) -> ""
+   | None -> ""
+
+
+let getDate (pdf:Pdf.t) : string =
+
+   (* format:
+    * D:YYYYMMDDHHmmSSOHH'mm'
+    * with optionalness: D:YYYY[MM[DD[HH[mm[SS[O[HH'[mm']]]]]]]] *)
+   let s = lookupInfoUtf8 pdf "/CreationDate" in
+
+   (* take yearmonthday, or just year *)
+   if (String.length s) >= 10
+   then
+      String.sub s 2 8
+   else if (String.length s) >= 6
+   then
+      String.sub s 2 4
+   else
+      ""
+
+
+let getPagecount (pdf:Pdf.t) : int =
+
+   Pdfpage.endpage pdf ;;
+
+
+
+
 (* ---- public functions ---- *)
 
-let extractTadist (_(*pdfPathname*):string) : (Tadist.nameStructRaw option) ress =
+let extractTadist (pdfPathname:string) : (Tadist.nameStructRaw option) ress =
 
-   Error "PDF not implemented yet"
+   match recognisePdf pdfPathname with
+   | Error _ as e -> e
+   | Ok false     -> Ok None
+   | Ok true      ->
+      try
+         let pdf = Pdfread.pdf_of_file None None pdfPathname in
 
-   (*
-   Ok None -- not recognised
-   Ok Some ... -- recognised, data
+         Ok (Some Tadist.{
+            titleRaw  = [ lookupInfoUtf8 pdf "/Title" ] ;
+            authorRaw = [ lookupInfoUtf8 pdf "/Author" ] ;
+            dateRaw   = [ getDate pdf ] ;
+            idRaw     = [] ;
+            subtypRaw = (string_of_int (getPagecount pdf)) ^ "p" ;
+            typRaw    = _TYPE ;
+         } )
 
-   ? recognised as a PDF
-      - no: Ok None
-      - yes:
-         ? read PDF
-            - fail: Error ...
-            - ok:
-               - get TAD and pagecount (or not/empty)
-                  - get TAD
-                     ? get /Info
-                        - fail: "" , "" , ""
-                        - ok:
-                           - get /Title , /Author , /CreationDate
-                  - get pagecount
-               - get isbn from body (or not/empty)
-               ? any TAD empty
-                  - yes:
-                     - lookup data from isbn
-                        - openlibrary.org ...
-               - Ok (Some Tadist.( {
-                  titleRaw  = title ;
-                  authorRaw = author ;
-                  dateRaw   = date ;
-                  idRaw     = isbn ;
-                  subtypRaw = "p" ^ (string_of_int pagecount) ;
-                  typRaw    = _TYPE } ) )
-
-   todo:
-   * recognisePdf
-      * first bytes: %PDF–1.x(endline)
-   *)
+      with
+      | _ -> Error "PDF read failed"
